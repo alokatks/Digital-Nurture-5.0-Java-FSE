a87d304259df3cc669bf0ebc0481d74430c1e85e
Alok Tiwari <alok08022006@gmail.com>
2026-07-24 22:15:54 +0530

    Add hands-on-3: React blogapp components

---
+++ week5/hands-on-3/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Posts from './Posts';
+
+function App() {
+  return (
+    <div>
+         <post />
+      <Posts />
+   
+    </div>
+  );
+}
+
+export default App;
